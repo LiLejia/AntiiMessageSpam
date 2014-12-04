130dfc5d8f237ccea89b7248f304e6999c65a89e
Henry Lee <leelejia@gmail.com>
2014-12-04 17:56:54 +0800

    添加了一点点逻辑

--- EmailSpam/ShareViewController.m
+++ EmailSpam/ShareViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "ShareViewController.h"
+#import <MobileCoreServices/MobileCoreServices.h>
 
 @interface ShareViewController ()
-
+@property (nonatomic,strong) NSMutableArray *uploadImageArray;
 @end
 
 @implementation ShareViewController
@@ -22,22 +23,14 @@
 - (void)didSelectPost {
     // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
     
-    for(NSExtensionItem *item in self.extensionContext.inputItems){
-        NSLog(@"fetched extention item:%@",item);
-        
-        NSArray *itemProviders = [item.userInfo objectForKey:NSExtensionItemAttachmentsKey];
-        for(NSItemProvider *provider in itemProviders){
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                [provider loadItemForTypeIdentifier:@"" options:nil completionHandler:^(id<NSSecureCoding> provider,NSError *error){
-                    
-                }];
-                
-            });
-        }
-    }
+    [self sendImage];
     
     // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
     [self.extensionContext completeRequestReturningItems:@[] completionHandler:nil];
+}
+
+- (void)sendImage{
+    
 }
 
 - (NSArray *)configurationItems {
@@ -46,7 +39,27 @@
 }
 
 - (void)presentationAnimationDidFinish{
-    
+    for(NSExtensionItem *item in self.extensionContext.inputItems){
+        NSArray *itemProviders = [item.userInfo objectForKey:NSExtensionItemAttachmentsKey];
+        for(NSItemProvider *provider in itemProviders){
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                [provider loadItemForTypeIdentifier:(NSString *)kUTTypeImage options:nil completionHandler:^(id<NSSecureCoding> provider,NSError *error){
+                    if(error){
+                        NSLog(@"fetch image :%@",error);
+                    }
+                    NSURL *imageURL = (NSURL *)provider;
+                    NSLog(@"image URL:%@",imageURL);
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        if(!self.uploadImageArray){
+                            self.uploadImageArray = [NSMutableArray array];
+                        }
+                        [self.uploadImageArray addObject:imageURL];
+                    });
+                }];
+                
+            });
+        }
+    }
 }
 
 - (void)didSelectCancel{
